2d30f74ebfe2729ecff82482c59ea5dd73e319e9
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-02 20:27:33 -0300

    fix: mudança na nomenclatura de url

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('sobre/', views.sobre, name='sobre'),
+    path('about/', views.about, name='sobre'),
     path('servicos/', views.servicos, name='servicos'),
     path('contato/', views.contato, name='contato'),
 ]
