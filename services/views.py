4e9cffc2b2de7fe977d3af7283c76ea3f48525e6
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-04 21:11:14 -0300

    feat: add base da views do service

--- services/views.py
+++ services/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# services/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Service
 
-# Create your views here.
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, 'services/list.html', {'services': services})
+
+def service_detail(request, slug):
+    service = get_object_or_404(Service, slug=slug)
+    return render(request, 'services/detail.html', {'service': service})
