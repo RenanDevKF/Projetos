4c6a661064a49d9976a0dab1b96b7174bf1b7a1f
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-04 21:09:52 -0300

    feat: add models base do service

--- services/models.py
+++ services/models.py
@@ -1,3 +1,21 @@
+# services/models.py
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    short_description = models.TextField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.IntegerField(help_text="Duração em minutos")
+    image = models.ImageField(upload_to='services/')
+    is_featured = models.BooleanField(default=False)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
