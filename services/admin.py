768a1d9ba8146ba3d6d172350504144f65964c01
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-04 21:10:25 -0300

    feat: add admin base do service

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,10 @@
+# services/admin.py
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'duration', 'is_featured')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('is_featured',)
+    search_fields = ('name', 'description')
