f07744fd207e3a26cf95e76e42afe2eee8c492c1
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-15 21:53:30 -0300

    fix: correção de bug retirando a area service do view em core

--- core/views.py
+++ core/views.py
@@ -17,8 +17,6 @@ def about(request):
 def contact(request):
     return render(request, 'core/contact.html')
 
-def services(request):
-    return render(request, 'core/services.html')
 
 def blog(request):
     return render(request, 'core/blog.html')
