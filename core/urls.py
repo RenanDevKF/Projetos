a814a59ba7be26bec989f17094d24406a9840183
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-15 21:53:09 -0300

    fix: bug de area service duplicada com aplicativo service

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),  # Página inicial
     path('about/', views.about, name='about'),  # Página "Sobre"
     path('contact/', views.contact, name='contact'),
-    path('services/', views.services, name='services'),
     path('blog/', views.blog, name='blog'),
     path('blog/<int:post_id>/', views.post_detail, name='post_detail'),
     path('booking/', views.booking_view, name='booking'),
