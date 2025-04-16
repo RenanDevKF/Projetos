108668854cbba9fdb41b421cd386088acb648426
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-15 21:54:00 -0300

    fix: correção do nome do caminho

--- services/urls.py
+++ services/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'services'
 
 urlpatterns = [
-    path('', views.service_list, name='list'),
+    path('', views.service_list, name='service_list'),
     path('<slug:slug>/', views.service_detail, name='detail'),
 ]
