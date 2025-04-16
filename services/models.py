00ee563bb266266f05f9ff9fefc935f016bc8576
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-16 20:17:34 -0300

    feat: add validaçãod e imagem ao painel admin

--- services/models.py
+++ services/models.py
@@ -1,7 +1,17 @@
 # services/models.py
 from django.db import models
 from django.utils.text import slugify
+from django.core.exceptions import ValidationError
 
+
+def validate_image_size(image):
+    min_width = 800
+    min_height = 600
+
+    if image.width < min_width or image.height < min_height:
+        raise ValidationError(
+            f'A imagem deve ter pelo menos {min_width}x{min_height} pixels'
+        )
 class Service(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, blank=True)
@@ -9,13 +19,18 @@ class Service(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     duration = models.IntegerField(help_text="Duração em minutos")
-    image = models.ImageField(upload_to='services/')
+    image = models.ImageField(
+        upload_to='services/',
+        validators=[validate_image_size],
+        help_text='Imagem recomendada: 800x600px (proporção 4:3)'
+    )
     is_featured = models.BooleanField(default=False)
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
-    
+
     def __str__(self):
         return self.name
+
